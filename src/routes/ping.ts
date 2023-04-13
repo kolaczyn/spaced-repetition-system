1e1b56b6f85eb9bfb7039d6fd8119d79dae40e58
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-04-13 22:57:02 +0200

    evil commit

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -3,7 +3,7 @@ import { Router } from "https://deno.land/x/oak@v12.1.0/mod.ts";
 export const pingRoute = () => {
   const router = new Router();
 
-  router.get("/", (ctx) => {
+  router.post("/", (ctx) => {
     ctx.response.body = "Pong";
   });
   return router;
