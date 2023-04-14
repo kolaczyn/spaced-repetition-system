51a0f56109a8dca1ee476cc0b93709a272815168
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-04-14 22:16:53 +0200

    use BDD

--- integration/cards.test.ts
+++ integration/cards.test.ts
@@ -4,19 +4,20 @@ import { getApp } from "../src/app.ts";
 import { Card, cardOne, cardThree, cardTwo, NOW } from "./mock_cards.ts";
 import { timeSimulation } from "../src/test/time_simulation.ts";
 import { assertEquals } from "https://deno.land/std@0.160.0/testing/asserts.ts";
-
-const app = await getApp(() => NOW);
+import { beforeEach, it } from "https://deno.land/std@0.155.0/testing/bdd.ts";
 
 const deleteCards = async () => {
   const request = await superoak(app);
   await request.delete("/v2/cards").expect(200);
 };
 
-Deno.test("delete cards to have a clean slate", async () => {
+beforeEach(async () => {
   await deleteCards();
 });
 
-Deno.test("insert and get card", async () => {
+const app = await getApp(() => NOW);
+
+it("inserts a card and gets the card", async () => {
   const request1 = await superoak(app);
   const response = await request1.post("/v2/cards").send({
     question: cardOne.question,
@@ -34,17 +35,17 @@ Deno.test("insert and get card", async () => {
   });
 });
 
-Deno.test("get not existing card", async () => {
+it("get not existing card", async () => {
   const request = await superoak(app);
   await request.get("/v2/cards/9999").expect(404);
 });
 
-Deno.test("card with id which is not a number", async () => {
+it("card with id which is not a number", async () => {
   const request = await superoak(app);
   await request.get("/v2/cards/abc").expect(400);
 });
 
-Deno.test("insert invalid", async () => {
+it("insert invalid", async () => {
   const request = await superoak(app);
   await request.post("/v2/cards").send({
     question: 20000,
@@ -54,14 +55,15 @@ Deno.test("insert invalid", async () => {
   }).expect(400);
 });
 
-Deno.test("delete cards to have a clean slate", async () => {
-  await deleteCards();
-});
-
 const { getNow } = timeSimulation();
 const anotherApp = await getApp(getNow);
 
-Deno.test("get all cards", async () => {
+it("gets all cards", async () => {
+  const request0 = await superoak(app);
+  await request0.get("/v2/cards").expect(200).expect((x) => {
+    assertEquals(x.body.length, 0);
+  });
+
   const request1 = await superoak(anotherApp);
   await request1.post("/v2/cards").send(cardOne).expect(201);
 
