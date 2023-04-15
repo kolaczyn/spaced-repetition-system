bba3a920a2907f7a7cce5f0853e66a800e7ab80c
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-04-15 12:37:29 +0200

    fix cors error

--- integration/ping_pog.test.ts
+++ integration/ping_pog.test.ts
@@ -1,10 +1,21 @@
 import "https://deno.land/std@0.182.0/dotenv/load.ts";
 import { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
 import { getApp } from "../src/app.ts";
+import { assertEquals } from "https://deno.land/std@0.160.0/testing/asserts.ts";
 
 const app = await getApp();
 
 Deno.test("ping pong", async () => {
   const request = await superoak(app);
   await request.get("/v1/ping").expect(200).expect("Pong");
+});
+
+Deno.test("It should add CORS headers to the response", async () => {
+  const request = await superoak(app);
+  await request.get("/v1/ping").expect(200).expect((x) => {
+    assertEquals(
+      x.headers["access-control-allow-origin"],
+      "http://localhost:4200",
+    );
+  });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.1.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { pingRoute } from "./routes/ping.ts";
 import { cardsRoute } from "./routes/cards.ts";
 import { getClient } from "./db/card_postgres.db.ts";
@@ -12,5 +13,9 @@ export const getApp = async (getNow = Date.now) => {
   // TODO make this v1
   router.use("/v2/cards", cardsRoute(dbClient, getNow).routes());
 
-  return new Application().use(router.routes()).use(router.allowedMethods());
+  return new Application().use(oakCors({
+    origin: "http://localhost:4200",
+  })).use(
+    router.routes(),
+  ).use(router.allowedMethods());
 };
